b666440f1e06d659608d395e36581c72e8afd9d4
yld-weng <59968766+yld-weng@users.noreply.github.com>
2020-06-04 12:19:35 +0100

    update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
     siteUrl: "https://dataviz.shef.ac.uk"
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID,
+        head: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -97,41 +104,11 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (e.g. <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (e.g. for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
               classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
               inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
               aliases: {},
-              // This toggles the display of line numbers globally alongside the code.
-              // To use it, add the following line in gatsby-browser.js
-              // right after importing the prism color scheme:
-              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-              // Defaults to false.
-              // If you wish to only show line numbers on certain code blocks,
-              // leave false and use the {numberLines: true} syntax below
               showLineNumbers: true,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: false,
-              // This adds a new language definition to Prism or extend an already
-              // existing language definition. More details on this option can be
-              // found under the header "Add new language definition or extend an
-              // existing language" below
             },
           },
           {
@@ -320,35 +297,6 @@ module.exports = {
   //        }
   //     }
   //  },
-   {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GA_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 1000,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-      },
-    },
 
   ],
 }
--- src/components_blog/paginationPost.jsx
+++ src/components_blog/paginationPost.jsx
@@ -11,10 +11,11 @@ class PaginationPost extends React.Component {
   }
 
   render() {
-    const { mdx } = this.props
+    const { mdx, share, prev, next } = this.props
+
     let pruneLen = 40
-    let prevTitle = this.props.prev ? this.props.prev.node.frontmatter.title : ""
-    let nextTitle = this.props.next ? this.props.next.node.frontmatter.title : ""
+    let prevTitle = prev ? prev.node.frontmatter.title : ""
+    let nextTitle = next ? next.node.frontmatter.title : ""
     
     if(prevTitle.length > pruneLen){
       prevTitle = prevTitle.substring(0,pruneLen).concat(" ...")
@@ -26,10 +27,10 @@ class PaginationPost extends React.Component {
     return(
       <div className="bg-gray-900">
         <div className="flex flex-wrap text-sm justify-center w-full py-2 lg:hidden">
-          <Twitter className="bg-gray-500 hover:bg-highlight_2" solid small message={`${mdx.frontmatter.title} - ${mdx.frontmatter.description}`} link={`https://${window.location.host}${mdx.fields.slug}`} />
-          <Facebook className="bg-gray-500 hover:bg-highlight_2" solid small link={`https://${window.location.host}${mdx.fields.slug}`} />
-          <Mail className="bg-gray-500 hover:bg-highlight_2" solid small subject={`${mdx.frontmatter.title} - ${mdx.frontmatter.description}`} link={`https://${window.location.host}${mdx.fields.slug}`} />
-          <Linkedin className="bg-gray-500 hover:bg-highlight_2" solid small message={`${mdx.frontmatter.title} - ${mdx.frontmatter.description}`} link={`https://${window.location.host}${mdx.fields.slug}`} />
+          <Twitter className="greyScale-100 hover:greyScale-0" solid small message={share[0]} link={share[1]} />
+          <Facebook className="greyScale-100 hover:greyScale-0" solid small link={share[1]} />
+          <Mail className="hover:bg-red-600" solid small subject={share[0]} link={share[1]} />
+          <Linkedin className="greyScale-100 hover:greyScale-0" solid small message={share[0]} link={share[1]} />
         </div> 
         <div className="flex flex-wrap text-gray-200 justify-center items-center content-center mx-auto pt-6 text-sm">
           {mdx.frontmatter.category.map((cat) => (
@@ -41,14 +42,14 @@ class PaginationPost extends React.Component {
         </div>
 
         <div className="flex justify-center py-8 mx-auto font-semibold" >
-          {this.props.prev && (
-            <a value="prev" className="flex items-center pr-6 hover:text-highlight_2 text-gray-300 min-w-20" href={this.props.prev.node.fields.slug}>
+          {prev && (
+            <a value="prev" className="flex items-center pr-6 hover:text-highlight_2 text-gray-300 min-w-20" href={prev.node.fields.slug}>
               <TiArrowLeftThick className="mr-3 text-xl" /> {prevTitle}
             </a>
           )}
 
-          {this.props.next && (
-            <a value="next" className="flex items-center pl-6 hover:text-highlight_2 text-gray-300 min-w-20" href={this.props.next.node.fields.slug}>
+          {next && (
+            <a value="next" className="flex items-center pl-6 hover:text-highlight_2 text-gray-300 min-w-20" href={next.node.fields.slug}>
               {nextTitle} <TiArrowRightThick className="ml-3 text-xl" />
             </a>
           )}
@@ -62,7 +63,8 @@ class PaginationPost extends React.Component {
 export default PaginationPost
 
 PaginationPost.propTypes = {
-  mdx: PropTypes.any,
-  prev: PropTypes.any,
-  next: PropTypes.any,
+  mdx: PropTypes.node,
+  prev: PropTypes.node,
+  next: PropTypes.node,
+  share: PropTypes.array,
 }
