271ee0437070c6be2b321dd9e42044cf8dd64ec6
yld-weng <59968766+yld-weng@users.noreply.github.com>
2020-06-26 18:25:22 +0100

    tag style

--- src/components_blog/menuTag.jsx
+++ src/components_blog/menuTag.jsx
@@ -20,10 +20,10 @@ const menuTag = ({ pageContext }) => {
 
 
   return (
-		<div className="w-full px-5 py-1 xl:py-2 text-gray-100 shadow-2xl text-sm xl:hidden relative z-10" style={{fontFamily: "TUoS Blake", backgroundColor: "rgba(0,0,0,0.80)"}}>
+		<div className="w-full px-5 py-1 xl:py-2 text-gray-100 shadow-lg text-sm xl:hidden relative z-10 bg-white">
       <div className={`${isOpen ? `` : ``} overflow-hidden pt-1 pb-2`}>
         <div>
-          <h1 className="inline-block text-2xl font-semibold mr-4"><FaTags style={{display: "inline-block"}} /> {location.href.includes("/blog/tag/") ? <Link to="/blog/#read">ALL</Link> : "Tags"}</h1>
+          <h1 className="inline-block text-2xl font-semibold mr-4 text-gray-900"><FaTags style={{display: "inline-block"}} /> {location.href.includes("/blog/tag/") ? <Link to="/blog/#read">ALL</Link> : "Tags"}</h1>
           <div className="inline-block focus:outline-none text-gray-600 bg-white shadow px-2 rounded-lg ml-2">
             <FiSearch className="inline-block text-center text-xl -mt-2" />
             <input id="tagSearch" onChange={handleChange}  className="search__input py-1 pl-2 text-base focus:outline-none pr-3 text-gray-600" style={{maxWidth: "40vw"}} type="text" name="search" placeholder="Search for tags" />
@@ -37,7 +37,7 @@ const menuTag = ({ pageContext }) => {
           ))}
         </div>
       </div>
-      <div onClick={() => toggleisOpen(!isOpen)} className='flex justify-center text-2xl absolute top-0 right-0 mr-4 mt-4'>
+      <div onClick={() => toggleisOpen(!isOpen)} className='text-gray-900 flex justify-center text-2xl absolute top-0 right-0 mr-4 mt-4'>
         <FaAngleDown className={` `} style={{transform: isOpen ? "rotate(180deg)" : "", transition: 'all .5s ease'}}  />
       </div>
     </div>
--- src/components_blog/menuTagSlide.jsx
+++ src/components_blog/menuTagSlide.jsx
@@ -22,16 +22,16 @@ const menuTagSlide = ({ pageContext }) => {
 
   return (
   
-		<div className="xl:w-1/4 2xl:w-1/5 px-5 py-1 xl:py-3 text-gray-100 shadow-2xl text-sm fixed top-0 right-0 min-h-100 hidden xl:block flex flex-row border-l-2 border-black transition duration-500 z-20" 
-      style={{fontFamily: "TUoS Blake", backgroundColor: "rgba(0,0,0,0.90)", transform: isOpen ? "translateX(0%)" : "translateX(101%)"}}
+		<div className="xl:w-1/4 2xl:w-1/5 px-5 py-1 xl:py-3 text-gray-100 shadow-lg text-sm fixed top-0 right-0 min-h-100 xl:block flex flex-row border-l-2 border-white transition duration-500 z-50 bg-white" 
+      style={{fontFamily: "TUoS Blake",  transform: isOpen ? "translateX(0%)" : "translateX(101%)"}}
     >
       <div onClick={() => toggleisOpen(!isOpen)} className='absolute left-0 min-h-100 -ml-10 font-bold cursor-pointer flex flex-row items-center transition duration-500 delay-500' style={{transform: 'translateY(-5%)', fontFamily: 'TUoS Blake'}}>
-        <div className={`${isOpen ? `hidden` : `block`} px-3 py-4 hover:bg-highlight_2 bg-gray-700 transition duration-500 shadow-xl`}>T<br />A<br />G</div>
-        <div className={`${isOpen ? `block` : `hidden`} px-3 py-4 bg-highlight_2 hover:bg-gray-700 transition duration-500 shadow-xl font-bold`}><IoMdClose /></div>
+        <div className={`${isOpen ? `hidden` : `block`} px-3 py-4 hover:bg-highlight_2 bg-gray-900 transition duration-500 shadow-xl`}>T<br />A<br />G</div>
+        <div className={`${isOpen ? `block` : `hidden`} px-3 py-4 bg-highlight_2 hover:bg-gray-900 transition duration-500 shadow-xl font-bold`}><IoMdClose /></div>
       </div>
       <div className="overflow-hidden lg:py-2 xl:py-6">
         <div>
-          <h1 className="inline-block text-2xl pb-2 xl:pb-5 font-semibold mr-4"><FaTags style={{display: "inline-block"}} /> {location.href.includes("/blog/tag/") ? <Link to="/blog/#read">ALL</Link> : "Tags"}</h1>
+          <h1 className="inline-block text-2xl pb-2 xl:pb-5 font-semibold mr-4 text-gray-900"><FaTags style={{display: "inline-block"}} /> {location.href.includes("/blog/tag/") ? <Link to="/blog/#read">ALL</Link> : "Tags"}</h1>
           <div className="inline-block focus:outline-none text-gray-600 bg-white shadow px-2 rounded-lg ml-6 xl:ml-2 mb-4">
             <FiSearch className="inline-block text-center text-xl -mt-2" />
             <input id="tagSearch" onChange={handleChange}  className="search__input py-1 pl-2 text-base focus:outline-none pr-3 text-gray-600" style={{minWidth: "5vw"}} type="text" name="search" placeholder="Search for tags" />
