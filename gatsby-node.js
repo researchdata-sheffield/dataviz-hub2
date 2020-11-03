aa947b9e073343b40b9048e0220af1f9f4285498
yld-weng <59968766+yld-weng@users.noreply.github.com>
2020-11-03 14:43:33 +0000

    fix canvas

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,29 @@
  */
 
 
-
-
 const { createFilePath } = require('gatsby-source-filesystem')
 const kebabCase = require(`lodash.kebabcase`)
 const path = require("path")
 
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  loaders,
+  actions,
+}) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+};
 
 /**
  *  Create file path for blog posts
