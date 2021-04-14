2312bc0abf60a7dd7e799b2e9d6ca3e3af2051e8
yld-weng <Y.Weng@sheffield.ac.uk>
2021-04-14 17:43:21 +0100

    Save for removal of mathematical equations

--- content/docs/2021-04-12-LearningPath-Statistical-Modeling-5/script.R
+++ content/docs/2021-04-12-LearningPath-Statistical-Modeling-5/script.R
@@ -1,28 +1,61 @@
 library(tidyverse)
 
-lm1 <- lm(mpg ~ hp , mtcars)
-lm2 <- lm(mpg ~ hp + gear, mtcars)
-
-summary(lm1)
-summary(lm2)
-
-anova(lm1, lm2)
-
-
 # Residual vs fitted: residual vs predicted
 # Normal Q-Q: distribution of errors, straight line if normally distributed
 # Scale-Location: Ideally be randomly scattered
 # Cook's distance: Indicates influence of each point on the model
+lm3 <- lm(mpg ~ disp, mtcars)
+
+par(mfrow=c(2,2))
+plot(lm3,1)
+plot(lm3,2)
+plot(lm3,3)
+plot(lm3,5)
 
 
-lm3 <- lm(Sepal.Length ~ Species, iris)
+
+meanArr <- c()
+sdArr <- c()
+par(mfrow=c(2,2))
+
+for (var in 1:50) {
+  sample1 <- sample_n(iris[iris$Species == 'setosa',], 20)
+  #sample2 <- sample_n(iris[iris$Species == 'versicolor',], 20)
+  #score <- var.test(sample1$Sepal.Length, sample2$Sepal.Length)
+  #print(score$estimate)
+  meanArr <- c(meanArr, mean(sample1$Sepal.Length))
+  sdArr <- c(sdArr, sd(sample1$Sepal.Length))
+}
+
+plot(meanArr, sdArr)
+abline(lm(sdArr ~ meanArr))
 
 
-plot(lm3)
+for (var in 1:50) {
+  sample <- sample_n(iris[iris$Species == 'versicolor',], 20)
+  meanArr <- c(meanArr, mean(sample$Sepal.Length))
+  sdArr <- c(sdArr, sd(sample$Sepal.Length))
+}
+
+plot(meanArr, sdArr)
+abline(lm(sdArr ~ meanArr))
 
 
-sample1 <- sample_n(mtcars, 20)
-sample2 <- sample_n(mtcars, 20)
+iris1 <- iris[iris$Species == "setosa",]
+iris2 <- iris[iris$Species == "versicolor",]
+iris3 <- iris[iris$Species == "virginica",]
 
-var.test(sample1, sample2)
+par(mfrow=c(2,2))
+plot(iris1$Sepal.Length, iris1$Sepal.Width)
+plot(iris2$Sepal.Length, iris2$Sepal.Width)
+plot(iris3$Sepal.Length, iris3$Sepal.Width)
 
+plot(lm(mpg ~ disp, mtcars))
+
+
+
+ggplot(iris, aes(x=Species, y=Sepal.Length, fill=Species)) + 
+  geom_boxplot(alpha=0.5) +
+  theme(legend.position="none")
+
+bartlett.test(Sepal.Length ~ Species, data = iris)
