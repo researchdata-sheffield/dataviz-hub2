aecd8e3e0cb105afa3e90f9d5e9ca084643721e2
Yuliang Weng <y.weng@sheffield.ac.uk>
2021-08-18 15:29:20 +0100

    feat: add support for twitter card
    Add twitter card type, image, title,
    and descriptions to blog posts, docs,
    and visualisations.

--- src/templates/docs/docsTemplateCustom.jsx
+++ src/templates/docs/docsTemplateCustom.jsx
@@ -10,33 +10,61 @@ import { useScript } from "../../utils/hooks/useScript"
 import { getShareLinks } from "../../utils/shared"
 import CommonMdxProvider from "../../components/shared/commonMdxProvider"
 
-
 const docsTemplateCustom = ({ data: { mdx }, pageContext }) => {
-  const {prev, next} = pageContext
-  const shareLinks = getShareLinks(mdx);
-  const d3 = mdx.frontmatter.d3 ? mdx.frontmatter.d3 : null;
-  
+  const { prev, next } = pageContext
+  const shareLinks = getShareLinks(mdx)
+  const d3 = mdx.frontmatter.d3 ? mdx.frontmatter.d3 : null
+
   // include d3 scripts
   useScript("https://unpkg.com/topojson@3", "", false)
-  {d3 && d3.map((d) => {
-    useScript(withPrefix(`d3/${d}`), "", false)
-  })}
+  {
+    d3 &&
+      d3.map((d) => {
+        useScript(withPrefix(`d3/${d}`), "", false)
+      })
+  }
 
   return (
     <>
-      <SEO 
-      title={mdx.frontmatter.title} 
-      keywords={["the university of sheffield", "data visualisation", "data visualisation hub", "research", "docs", mdx.frontmatter.title, ...mdx.frontmatter?.tag || [], ...mdx.frontmatter?.category || []]} 
+      <SEO
+        title={mdx.frontmatter.title}
+        keywords={[
+          "the university of sheffield",
+          "data visualisation",
+          "data visualisation hub",
+          "research",
+          "docs",
+          mdx.frontmatter.title,
+          ...(mdx.frontmatter?.tag || []),
+          ...(mdx.frontmatter?.category || [])
+        ]}
+        author={mdx.frontmatter.author.map((a) => a.name).join(", ")}
+        description={mdx.frontmatter?.description}
+        twitterImage={
+          mdx.frontmatter?.thumbnail?.childImageSharp?.gatsbyImageData
+        }
+        twitterImageAlt={`Thumbnail image for the docs - ${mdx.frontmatter?.title}`}
       />
       <Helmet>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" type='text/javascript' /> 
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+          type="text/javascript"
+        />
       </Helmet>
 
       <div className="justify-center mx-auto text-lg lg:text-xl">
         <CommonMdxProvider mdx={mdx} />
       </div>
-      
-      <PostPagination mdx={mdx} prev={prev} next={next} shareLinks={shareLinks} displayTags={false} />
+
+      <PostPagination
+        mdx={mdx}
+        prev={prev}
+        next={next}
+        shareLinks={shareLinks}
+        displayTags={false}
+      />
       <Comment mdx={mdx} />
     </>
   )
@@ -44,17 +72,14 @@ const docsTemplateCustom = ({ data: { mdx }, pageContext }) => {
 
 export default docsTemplateCustom
 
-
 docsTemplateCustom.propTypes = {
   data: PropTypes.any,
-  pageContext: PropTypes.any,
+  pageContext: PropTypes.any
 }
-
-
 
 export const docsCustomQuery = graphql`
   query docsQuery_custom($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
       id
       body
       tableOfContents
