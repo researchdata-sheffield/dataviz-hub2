7f46d4215bb0f5b3d2286e4d46ffbb7a70dafd9f
Yuliang Weng <y.weng@sheffield.ac.uk>
2021-09-07 15:44:35 +0100

    test: remove cypress

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   cacheDirectory: ".jest-cache",
   // transform js/jsx files using jest-preprocess.js file
   transform: {
-    "^.+\\.jsx?$": `<rootDir>/tests/jest-preprocess.js`,
+    "^.+\\.jsx?$": `<rootDir>/tests/jest-preprocess.js`
   },
   // handling mocking static file imports
   moduleNameMapper: {
@@ -10,25 +10,16 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`
   },
   // ignore these files when testing
-  testPathIgnorePatterns: [
-    "node_modules", 
-    "\\.cache", 
-    "<rootDir>.*/public",
-    "<rootDir>.*/cypress"
-  ],
+  testPathIgnorePatterns: ["node_modules", "\\.cache", "<rootDir>.*/public"],
   // IMPORTANT: because Gatsby includes un-transpiled ES6 code
   // And it will try to transform files within Gatsby-name folders under node_modules
   // gatsby-browser-entry.js isn’t being transpiled before running in Jest
   transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
   globals: {
-    __PATH_PREFIX__: "",
+    __PATH_PREFIX__: ""
   },
   testURL: "http://localhost",
   // files will be included before all tests are run
-  setupFiles: [
-    "<rootDir>/tests/loadershim.js"
-  ],
-  setupFilesAfterEnv: [
-    "<rootDir>/tests/setupAfterEnv.js"
-  ]
-}
+  setupFiles: ["<rootDir>/tests/loadershim.js"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setupAfterEnv.js"]
+};
