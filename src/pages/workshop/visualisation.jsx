605290a52dc3cd3bf34582b12cb1bcee07240ad2
Yuliang Weng <y.weng@sheffield.ac.uk>
2021-09-24 14:10:14 +0100

    vis: complete visualisation

--- src/pages/workshop/visualisation.jsx
+++ src/pages/workshop/visualisation.jsx
@@ -1,10 +1,4 @@
 import React from "react";
-//import styled from "styled-components";
-import Chicken from "./images/chicken.svg";
-import Rice from "./images/grain-rice.svg";
-import Mushroom from "./images/mushroom.svg";
-import Seafood from "./images/seafood.svg";
-import Water from "./images/water-droplet.svg";
 
 const sourceInfo = {
   position: "absolute",
@@ -14,10 +8,6 @@ const sourceInfo = {
   color: "#fff",
   lineHeight: 1,
   whiteSpace: "nowrap"
-};
-
-const imageStyle = {
-  margin: "0 10px"
 };
 
 /**
@@ -40,77 +30,6 @@ const Visualisation = () => {
         position: "relative"
       }}
     >
-      <h1
-        style={{
-          color: "#fff",
-          lineHeight: 1.1,
-          fontSize: "2.3rem",
-          fontWeight: "700",
-          marginBottom: "2rem",
-          paddingBottom: ".5rem"
-        }}
-      >
-        Improved rice cooking approach to{" "}
-        <span style={{ color: "#EE9381" }}>maximise arsenic removal</span> while
-        preserving NUTRIENT elements
-      </h1>
-      <div
-        style={{
-          padding: "1rem 0",
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between"
-        }}
-      >
-        <div
-          style={{
-            width: "30%",
-            padding: ".5rem",
-            background: "#742110",
-            textAlign: "center",
-            borderRadius: ".375rem",
-            color: "#DBDBDB",
-            fontSize: ".79rem",
-            fontWeight: "600",
-            boxShadow:
-              "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)"
-          }}
-        >
-          <h3>33</h3>
-          <h1 style={{ fontWeight: "800", fontSize: "2.1rem" }}>As</h1>
-          <h3>Arsenic</h3>
-          <h3>74.922</h3>
-        </div>
-        <div
-          style={{
-            width: "65%",
-            color: "#DBDBDB",
-            alignSelf: "center",
-            fontSize: ".87rem"
-          }}
-        >
-          <span style={{ fontSize: "1.2rem" }}>&#128161;</span> Arsenic is
-          classified as a Group-A carcinogen that can cause lung and skin
-          cancer.
-        </div>
-      </div>
-      <h1
-        style={{
-          color: "#DBDBDB",
-          textAlign: "center",
-          margin: "1rem 0 .5rem 0",
-          fontSize: ".87rem"
-        }}
-      >
-        It can be found in ...
-      </h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <img style={{ ...imageStyle, width: "35px" }} src={Chicken} />
-        <img style={{ ...imageStyle, width: "50px" }} src={Seafood} />
-        <img style={{ ...imageStyle, width: "75px" }} src={Rice} />
-        <img style={{ ...imageStyle, width: "50px" }} src={Water} />
-        <img style={{ ...imageStyle, width: "35px" }} src={Mushroom} />
-      </div>
       <h1
         style={{ fontWeight: 800, left: 0, fontSize: ".9rem", ...sourceInfo }}
       >
