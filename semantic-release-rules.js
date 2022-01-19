b1a1ff096dbb2d8c591a33794ccdbec180551bde
Yuliang Weng <59968766+yld-weng@users.noreply.github.com>
2022-01-19 17:12:05 +0000

    build: add semantic-release package to the codebase

---
+++ semantic-release-rules.js
@@ -0,0 +1,17 @@
+module.exports = [
+  { type: "feat", release: "minor" },
+  { type: "fix", release: "patch" },
+  { type: "perf", release: "patch" },
+  { type: "docs", scope: "new-*", release: "minor" },
+  { type: "docs", release: "patch" },
+  { type: "refactor", scope: "core-*", release: "minor" },
+  { type: "refactor", release: "patch" },
+  { type: "style", release: "patch" },
+  { type: "test", release: "patch" },
+  { type: "build", release: "patch" },
+  { type: "ci", release: "patch" },
+  { type: "chore", release: "patch" },
+  { breaking: true, release: "major" },
+  { revert: true, release: "patch" },
+  { scope: "no-release", release: false }
+]
