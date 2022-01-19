87542e5e6b9ae721a780188be65c722ce0747bfb
Yuliang Weng <59968766+yld-weng@users.noreply.github.com>
2022-01-19 15:57:08 +0000

    feat: add the changelog page and the markdown file
    New component for export common MDX components.
    Add some past commit messages into the CHANGELOG.mdx file.

---
+++ src/pages/changelog.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import SEO from "../components/shared/seo";
+import { MDXProvider } from "@mdx-js/react";
+import ChangelogMDX from "../../CHANGELOG.mdx";
+import { commonMdxComponents } from "../components/style/commonMdxComponent";
+
+const Changelog = () => {
+  return (
+    <>
+      <SEO
+        title="Changelog"
+        keywords={[
+          "the university of sheffield",
+          "data visualisation",
+          "data visualisation hub",
+          "research",
+          "changelog",
+          "What's new"
+        ]}
+      />
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+          padding: "180px 0 50px"
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "3rem",
+            fontWeight: "800",
+            letterSpacing: ".4rem"
+          }}
+        >
+          CHANGELOG
+        </h1>
+        <h3 style={{ fontSize: "1.5rem", color: "#595959" }}>
+          🥳 What&apos;s new?
+        </h3>
+      </div>
+      <div
+        style={{
+          maxWidth: "800px",
+          margin: "auto",
+          paddingBottom: "100px",
+          paddingTop: "50px"
+        }}
+      >
+        <MDXProvider components={{ ...commonMdxComponents }}>
+          <ChangelogMDX />
+        </MDXProvider>
+      </div>
+    </>
+  );
+};
+
+export default Changelog;
