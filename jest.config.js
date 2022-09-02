977f427bcbc4317725dfe0b4382214e13719a050
Yuliang Weng <59968766+yld-weng@users.noreply.github.com>
2022-09-02 11:20:22 +0100

    build(dependencies): upgrade

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,11 @@
 module.exports = {
-  preset: "jest-playwright-preset",
   cacheDirectory: ".jest-cache",
 
   // transform js/jsx files using jest-preprocess.js file
   transform: {
     "^.+\\.jsx?$": `<rootDir>/tests/jest-preprocess.js`
   },
-
-  // handling mocking static file imports
-  moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
-    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
-    "^gatsby-core-utils/(.*)$": `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
-    "^gatsby-plugin-utils/(.*)$": [
-      `gatsby-plugin-utils/dist/$1`,
-      `gatsby-plugin-utils/$1`
-    ] // Workaround for https://github.com/facebook/jest/issues/9771
-  },
+  
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test|e2e).[jt]s?(x)"
